caa365809e00ae203c238e56184ae5667bf3021c
Vikrant Jain <47775025+TheTimeSawyer@users.noreply.github.com>
2019-03-23 14:40:01 +0530

    Update GraphCode.py

--- GraphCode.py
+++ GraphCode.py
@@ -1,33 +1,47 @@
-class AdjNode(object):
-    def __init__(self,lat,longi):
+class Node(object):
+    def __init__(self,ind,lat,long,cnodes):
+        self.ind=ind
         self.lat=lat
-        self.longi=longi
-        self.next=None
+        self.long=long
+        self.cnodes=cnodes.copy()
     
     def __str__(self):
-        return(str(self.lat)+" + "+str(self.longi)+"+"+self.cnodes)
+        return(str(self.lat)+"  "+str(self.long)+"  "+str(self.cnodes))
         
     def dist(self,other):
-        x=((self.lat-other.lat)*111000)
-        
-        ''' 111000m is Approximate for Regions around Agra,
-        where this vehicle is being tested'''
-        
-        longdist= (math.cos((self.lat)*math.pi/180))*111.321
-        
-        '''This is a formula for measuring approximate
-        distances between longitudes. This is valid only 
-        when tha latitudes of initial and final position 
-        are not very far apart.'''
-        
-        y=((self.longi-other.longi)*longdist)
-        return pow((x**2)-(y**2),0.5)
-        
-        '''The final distanceis calculated using the 
-        distance formula in coordinate geometry.'''
+        if other.ind in self.cnodes:
+            
+            x=((self.lat-other.lat)*111000)
+            
+            ''' 111000m is Approximate for Regions around Agra,
+            where this vehicle is being tested'''
+            
+            longdist= (math.cos((self.lat)*math.pi/180))*111.321
+            
+            '''This is a formula for measuring approximate
+            distances between longitudes. This is valid only 
+            when tha latitudes of initial and final position 
+            are not very far apart.'''
+            
+            y=((self.long-other.long)*longdist)
+            return pow((x**2)-(y**2),0.5)
+        else:            
+            return None
 
 
+# dummy node             
+class AdjNode: 
+    def __init__(self, data): 
+        self.vertex = data 
+        self.next = None
+# Random values of latitude and longitude.
+n0 = Node(0,65.5,128.26,[1])
+n1 = Node(1,65.5,128.26,[0,2,3])
+n2 = Node(2,65.5,128.26,[1])
+n3 = Node(3,65.5,128.26,[2,4])
+n4 = Node(4,65.5,128.26,[3])
 
+lnodes = [n0,n1,n2,n3,n4]
 
 class Graph: 
     def __init__(self, vertices): 
@@ -55,5 +69,25 @@ class Graph:
             while temp: 
                 print(" -> {}".format(temp.vertex), end="") 
                 temp = temp.next
-            print(" \n")
+            print(" \n") 
+''' This code creates a graph of indices and a 
+separate list which stores the data of each node(latitude
+and longitude). The '''            
 
+
+
+
+g = Graph(5)
+
+#How to create a graph by specifying its edges.
+#This is an example, specific to our chosen path.
+
+g.add_edge(0,1)
+g.add_edge(1,2)
+g.add_edge(1,3)
+g.add_edge(3,4)
+
+
+g.print_graph()
+#How to access the node data from the nodes list.
+print (lnodes[0])
